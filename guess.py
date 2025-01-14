9b525ff56f1d520fa8247cafeb0ddb224bddde30
Jacob Dahl <D00353336@utahtech.edu>
2025-01-14 15:48:37 -0700

    first commit

---
+++ guess.py
@@ -0,0 +1,13 @@
+import random
+
+print("Hi! Im' going to try to guess your age.")
+name = input("whats is your name? ")
+
+guessed = False
+while(guessed == False):
+    guess = random.randint(15,30)
+    user_response = input("Are you "+str(guess)+ "years old? ")
+    if user_response == 'y' or user_response == 'Y':
+        print(f"Hahaha! {name} is " + str(guess)+ " years old! I geussed it!")
+    else:
+        print("Darn.")
